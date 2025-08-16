dbf24b999591e4025c7ca194071d080598e35afe
Priyanshu Tiwari <Priyanshutiwari878@yahoo.com>
2025-08-16 11:50:57 +0530

    fix routes

--- app.py
+++ app.py
@@ -11,11 +11,11 @@ from pydantic import BaseModel
 # movie_index = saved["movie_index"] 
 
 # movies_df = pd.read_csv("movie_copy_2000s.tsv", sep=",")  # try comma first
+
 app = FastAPI(title="Cinephile Recommender")
 
 class MovieRequest(BaseModel):
     movie_title: str
-
 
 
 with open("movie_recommender_2.pkl", "rb") as f:
@@ -24,29 +24,26 @@ with open("movie_recommender_2.pkl", "rb") as f:
 model = data["model"]
 movie_index = data["movie_index"]
 best_final_df_with_only_movie_after_2000s = data["dataframe"]
-X = data["vectorizer"].transform(best_final_df_with_only_movie_after_2000s["combined"]) 
+X = data["vectorizer"].transform(best_final_df_with_only_movie_after_2000s["combined"])
 
-
-def recommend(title):
+def recommend(title: str):
     title = title.lower()
     if title not in movie_index:
-        return "Movie not found."
+        return {"recommended": [], "error": "Movie not found."}
 
     idx = movie_index[title]
-    distances, indices = model.kneighbors(X[idx], n_neighbors=5)
+    distances, indices = model.kneighbors(X[idx].reshape(1, -1), n_neighbors=5)
 
-    recommended = []
-    for i in indices[0][1:]:
-        recommended.append(best_final_df_with_only_movie_after_2000s.iloc[i]['title'])
-
-    return {"recommended":recommended}
+    recommended = [
+        best_final_df_with_only_movie_after_2000s.iloc[i]['title']
+        for i in indices[0][1:]
+    ]
+    return {"recommended": recommended}
 
 @app.get("/")
-def recommend_movie():
+def cornRoute():
     return {"Corn": "Successfully initiated"}
 
 @app.post("/recommend")
 def recommend_movie(req: MovieRequest):
-    title = req.movie_title.lower()
-    recommended = recommend(title)
-    return {"recommendations": recommended}
+    return recommend(req.movie_title)
